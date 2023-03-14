1223e1f4702499250c3258004264fe2829dac7c5
Banwind <a84012807@gmail.com>
2023-03-14 19:23:54 +0800

    feat: modify seeder.js

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -3,8 +3,15 @@ const urlsJSON = require('../../urls.json').results
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  for (let i=0; i < urlsJSON.length; i++) {
-    urlsModel.create({ ...urlsJSON[i]})
-  }
-  console.log('create seed done')
+  // 創建所有資料庫項目
+  const createPromises = urlsJSON.map(data => urlsModel.create({ ...data }))
+  Promise.all(createPromises)
+    .then(() => {
+      console.log('create seed done')
+      db.close()
+    })
+    .catch(error => {
+      console.error(error)
+      db.close()
+    })
 })
